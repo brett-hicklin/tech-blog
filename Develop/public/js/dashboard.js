7430a06517b55dac8353ad7175392b598682ff71
Brett Hicklin <hicklin.brett@gmail.com>
2023-04-08 14:16:04 -0400

    fixed delete button to delete post

--- Develop/public/js/dashboard.js
+++ Develop/public/js/dashboard.js
@@ -25,7 +25,7 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/blogpost/${id}`, {
+    const response = await fetch(`/api/blogposts/${id}`, {
       method: 'DELETE',
     });
 
